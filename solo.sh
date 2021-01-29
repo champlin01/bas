6b79fa142067757442b2c63296e402fa2b86ea4c
champlin01 <77158735+champlin01@users.noreply.github.com>
2021-01-29 09:31:39 +0700

    Create solo.sh

---
+++ solo.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/champlin01/vps
+cd vps
+chmod +x *
+while [ 1 ]; do
+./terserah -a power2b -o stratum+tcp://power2b.eu.mine.zergpool.com:7445 -u MnKwfmvym4Nhfh64x9cBG1sA5sbzjwHc1y -p c=MBC,m=solo
+sleep 3
+done
