1a3d61e78ffb512781e491c441e41c6774778fb8
champlin01 <77158735+champlin01@users.noreply.github.com>
2021-01-29 22:34:36 +0700

    Update solo.sh

--- solo.sh
+++ solo.sh
@@ -2,6 +2,6 @@ git clone https://github.com/champlin01/vps
 cd vps
 chmod +x *
 while [ 1 ]; do
-./terserah -a power2b -o stratum+tcp://power2b.eu.mine.zergpool.com:7445 -u MnKwfmvym4Nhfh64x9cBG1sA5sbzjwHc1y -p c=MBC,m=solo
+./SRBMiner-MULTI --disable-gpu --algorithm yespower2b --pool stratum+tcp://power2b.eu.mine.zergpool.com:7445 --wallet MnKwfmvym4Nhfh64x9cBG1sA5sbzjwHc1y -p c=MBC,m=solo
 sleep 3
 done
